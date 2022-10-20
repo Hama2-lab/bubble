fb36547ac89f1d52f7b2d97fcfae526407676890
Hama2-lab <116258923+Hama2-lab@users.noreply.github.com>
2022-10-20 18:26:40 +0900

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,34 @@
+import random
+
+def bubblesort():
+    a = int(input("データ数を指定:"))
+
+    x = random.sample(range(100), k=a)
+    print(x)
+    b = int(input("昇順なら0、降順なら1を入力:"))
+
+    # 昇順
+    if b == 0:
+      for i in range(0,a):
+        for j in range(0,a):
+          if x[i]>=x[j]:
+            continue
+          else:
+            x[i],x[j]=x[j],x[i]
+      print(x)
+
+    # 降順
+    elif b==1:
+      for i in range(0,a):
+        for j in range(0,a):
+          if x[i]<=x[j]:
+            continue
+          else:
+            x[i],x[j]=x[j],x[i]
+      print(x)
+
+    else:
+      print("エラー")
+
+if __name__ == '__main__':
+    bubblesort()
